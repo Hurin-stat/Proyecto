d243f3bc1c6934a18d558844f9666c82e9a692bc
Juan Velasquez M <jvelasquezm@unal.edu.co>
2020-08-03 10:10:39 -0500

    Bienvenida al proyecto.

---
+++ Leeme.R
@@ -0,0 +1,15 @@
+## ################################################## 
+## ##                                              ##
+## ##                   Buen dia                   ##
+## ##                                              ##
+## ##################################################
+## ##                                              ##
+## ##   Este es el repositorio del proyecto a      ##
+## ##   desarrollar, la idea es que todo lo rela-  ##
+## ##   cionado con el proyecto se comparta a      ##
+## ##   traves de este repositorio de github.      ##
+## ##                                              ##
+## ##                                              ##
+## ##                                              ##
+## ##                                              ##
+## ##                                              ##
